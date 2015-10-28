9f7f6097f1a99708f3517acfba7a385a0a412b51
pramodrhegde <pramod.rh@gmail.com>
2015-10-28 19:11:22 +0530

    added js to gh-pages

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 2000,
+    pause_on_hover: true,
+    animation_speed: 500,
+    navigation_arrows: true,
+    bullets: true
+  }
+});
